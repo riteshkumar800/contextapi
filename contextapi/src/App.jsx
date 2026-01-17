4bee0f8eeb0fdf684e6227c94e29ea408d8e872f
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-01-17 23:15:27 +0530

    code add

--- contextapi/src/App.jsx
+++ contextapi/src/App.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [todos, setTodos] = useState([])
+
+  const addTodo=(todo)=>{
+    setTodos((prev)=>[{id:Date.now(),...todo},...prev])
+
+  }
+
+  const updateTodo = (id,todo)=>{
+    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===id?todo:prevTodo)))
+  }
+
+  const deleteTodo=(id)=>{
+    setTodos((prev)=>prev.filter((prevTodo)=>prevTodo.id!=id))
+  }
+
+  const toggleComplete=(id)=>{
+    setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
+
+  }
+
+  useEffect(()=>{
+    
+  })
+
+
   return (
     <div>
       
