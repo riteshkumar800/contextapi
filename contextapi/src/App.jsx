f3fabb47bc5303b9d651b19f6681d2cac79f3354
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-01-17 23:26:26 +0530

    code added

--- contextapi/src/App.jsx
+++ contextapi/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
 
@@ -23,14 +23,41 @@ const App = () => {
   }
 
   useEffect(()=>{
-    
-  })
+    const todos=JSON.parse(localstorage.getItem("todos"))
+
+    if(todos && todos.length>0){
+      setTodos(todos)
+    }
+
+  },[])
+
+  useEffect(()=>{
+    localstorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
 
   return (
-    <div>
-      
-    </div>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
   )
 }
 
